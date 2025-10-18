cb17427eb60d848a319722c7f6b0f09c7533e907
EthanDai-aut <Autumn_dai4Ethan@outlook.com>
2025-10-18 20:26:08 +0800

    修改了所有代码

--- 一元一次方程.cpp
+++ 一元一次方程.cpp
@@ -1,23 +1,57 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include<iostream>
+#include<vector>
 using namespace std;
-//sRight表示为等式右边
-string sRight;
-//sLeft表示为等式左边
-string sLeft;
-int main() {
-    string s;
-    cin >> s;
-    cout << s << endl;
 
-    //这一步使sRight是等式的右边，sLeft是等式的左边
-    for (int i=0;i<s.length();i++) {
-        if (s[i] == '=') {
-            sRight = s.substr(0,i);
-            sLeft = s.substr(i+1,s.length()-i);
+vector<char> s;
+vector<int> numbers;
+vector<char> fuHao;
+char inputChar;
+
+
+int main()
+{
+    //输入算式
+	//==============================
+	cout << "Please input characters, end with '#':" << endl;
+    do {
+        cin >> inputChar;
+        if (inputChar != '#') {
+            s.push_back(inputChar);
+		}
+    }
+	while (inputChar != '#');
+
+    cout << "The inputing is complete." << endl;
+	//==============================
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        //判断式子的开头是否符号
+        if (i == 0 && (s[i] != '+' || s[i] != '-' || s[i] != '*' || s[i] != '*'))
+        {
+            s.insert(s.begin(),'+');
         }
     }
+	//输出算式
+    for (int i = 0; i < s.size(); i++)
+    {
+        cout << s[i];
+    }
+    int jx = -1;
+    int numberjx;
+	//提取符号和数字
+    for (int i = 0; i < s.size(); i++)
+    {
+        if ((s[i] == '+' || s[i] == '-') || (s[i] == '*' || s[i] == '/'))
+        {
+            jx++;
+			fuHao.push_back(s[jx]);
+        }
+        if ((s[i] >= '0' && s[i] <= '9') && (s[i-1] == '+' || s[i-1] == '-'))
+        {
+            numberjx = i;
+		}
 
-
+    }
+	return 0;
 }
